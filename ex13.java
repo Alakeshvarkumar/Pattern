dcd9afbc1013c375b3b71fe840e35bc60072b686
Alakesh <alakesh18052006@gmail.com>
2026-02-06 18:46:26 +0530

    Add initial implementation of patient surgery history page

---
+++ ex13.java
@@ -0,0 +1,10 @@
+class ex13{
+    public static void main(String[] args) {
+        int n=4;
+        int sp = n+2;
+        for(int i=1;i<=n;i++){
+            //number
+            
+        }
+    }
+}
