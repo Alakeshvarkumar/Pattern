f48dd1c7ae0e08f549b86a8f25018474b005376d
Alakesh <alakesh18052006@gmail.com>
2026-02-06 18:09:13 +0530

    Add comment to clarify loop condition in pyramid pattern implementation

--- ex9.java
+++ ex9.java
@@ -14,7 +14,7 @@ class ex9 {
             }
             System.out.println();
         }
-        for (int i = n-1; i > 0; i--) {
+        for (int i = n-1; i > 0; i--) {          // n-1 for non repetion of * 2 times
             for (int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
