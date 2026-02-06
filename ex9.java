c9d8cb9acc6fbeec0d6edbc2a068aabb2fe9430c
Alakesh <alakesh18052006@gmail.com>
2026-02-06 18:08:41 +0530

    Add implementation of diamond pattern in Java

---
+++ ex9.java
@@ -0,0 +1,28 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class ex9 {
+
+    public static void main(String args[]) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < 2 * i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n-1; i > 0; i--) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < 2 * i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
