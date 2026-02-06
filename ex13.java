b2e46be266ffdbe9b7f50478b689fb7cb62c4b35
Alakesh <alakesh18052006@gmail.com>
2026-02-06 19:03:34 +0530

    Refactor ex13.java to implement number and space printing logic for pattern generation

--- ex13.java
+++ ex13.java
@@ -4,7 +4,19 @@ class ex13{
         int sp = n+2;
         for(int i=1;i<=n;i++){
             //number
-            
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            //space
+            for(int j=1;j<=sp;j++){
+                System.out.print(" ");
+            }
+            //number
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            sp-=2;
+            System.out.println();
         }
     }
 }
