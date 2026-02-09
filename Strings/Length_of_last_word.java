f2cc528302db3a5b85cd9d44b853f8cdc76703da
Alakesh <alakesh18052006@gmail.com>
2026-02-09 17:57:26 +0530

    Add implementation of Length_of_last_word class to calculate the length of the last word in a string

---
+++ Strings/Length_of_last_word.java
@@ -0,0 +1,26 @@
+class Length_of_last_word {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int i = s.length() - 1;
+        int length = 0;
+
+        // remove trailing spaces by using this code snippet or trim fn
+        // while (i >= 0 && s.charAt(i) == ' ') {
+        //     i--;
+        // }
+
+        
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Length_of_last_word lc = new Length_of_last_word();
+        System.out.println(lc.lengthOfLastWord("Hello World"));
+    }
+}
